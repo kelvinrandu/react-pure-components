15fc26ce3ca5d4eb71a22c9e3a9e1713e9ba2bcd
randukelvin <randukelvin@gmail.com>
2020-09-30 16:08:01 +0300

    set up react project and added purecomp component

---
+++ pure-components/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import { PureComp } from './components/pureComp.tsx'
+
+function App() {
+  return (
+    <div className="App">
+        < PureComp />
+    </div>
+  );
+}
+
+export default App;
---
+++ pure-components/src/components/pureComp.tsx
@@ -0,0 +1,16 @@
+import React , {PureComponent} from 'react';
+
+export class PureComp extends PureComponent{
+  render(){
+    return(
+      <div> pure</div>
+      
+    )
+  }
+
+
+}
+
+
+
+export default PureComp;
